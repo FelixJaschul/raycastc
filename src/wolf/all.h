bf1dd7857c1101eb5f94a9c1250854a1d1472173
felixjaschul <Felix@Jaschul.de>
2025-05-01 22:48:25 +0200

    .

---
+++ src/wolf/all.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+#include <SDL2/SDL.h>
+
+#include "../color.h"
+#include "../mac.h"
+#include "../mov.h"
+#include "../screen.h"
+#include "../type.h"
+#include "state.h"
+
+#include "../map.c"
+#include "../player.c"
+#include "../render.c"
